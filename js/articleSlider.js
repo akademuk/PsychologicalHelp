b0017e155c6573c4b0859f47f837993d87438783
Oleg Fedorov <akademuk24@gmail.com>
2022-10-21 12:34:37 +0300

    sss

---
+++ js/articleSlider.js
@@ -0,0 +1,11 @@
+$(".articleSliderContainer").slick({
+    infinite: true,
+    dots:true,
+    arrow: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true,
+    centerMode: true,
+  });
+  
